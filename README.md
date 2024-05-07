fbaa37da525db2fea0ae10ee76e2588bda9caf6f
EdmondRen <ssbssb@live.cn>
2024-05-07 15:22:25 -0700

    Update init

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-# 2. Background generation
+# How to run background simulation
+## 1.1 MATHUSLA simulation workflow
+
+There are three steps in general,
+
+    Geant4 Simulation -> Digitizer -> Reconstruction
+
+Link to G4 simulation/Digitizer:
+
+Link to Reconstruction: 
+
+This repository implements helper scripts to run the entire workflow for background study, including
+
+1. Generating collider muon background with madgraph+pythia Geant4 simulation
+2. Generating cosmic ray background with parma, run through Geant4 simulation
+3. Running digitization. Two additional processes happen in this step:
+    a. The cosmic ray simulation events are randomly choosed to add to the collider background simulation
+    b. Detector noise are randomly added
+4. Running track and vertex reconstruction
+
+```
+1.Madgraph+Pythia -> G4Sim 
+                            \
+                             3.Digitize-> 4.Reconstruction
+                            /
+2.PARMA cosmic gen-> G4Sim
+```
+
+## 1.2 Steps
+
+First, make a directory and clone this repository. Then source the initialization script. 
+This script will setup the environment, compile the `Geant4 simulation` and `Digiziter` code if the executable is not presented.
+
+Second, run the script `run_all_bkg.sh` that implement the entire workflow described above.
+
+```bash
+mkdir background_gen
+cd !$
+
+git clone https://github.com/EdmondRen/Mu-Background.git
+cd Mu-Background
+
+source init.sh
+./run_all_bkg.sh
+```
+
+# 2. Details about the background under study
 ## 2.1 CMS background
 ## 2.2. Cosmic ray
 
--- init.sh
+++ init.sh
@@ -2,8 +2,43 @@ PATH_DATA="/project/rrg-mdiamond/data/MATHUSLA"
 
 
 #------------------------------------------------------------------
+# Dependencies
+# PYTHIA is in Tom's home directory
+# The rest is availabel from the preinstalled modules on cedar
+export PYTHIA8=/project/def-mdiamond/tomren/mathusla/pythia8308
+export PYTHIA8DATA=$PYTHIA8/share/Pythia8/xmldoc
+PATH=$PATH:/project/def-mdiamond/tomren/mathusla/dlib-19.24/install
+module load StdEnv/2020 gcc/9.3.0 qt/5.12.8 root/6.26.06  eigen/3.3.7 geant4/10.7.3 geant4-data/10.7.3
+
+#------------------------------------------------------------------
+# Get simulation, digitizer and tracker executables
+pushd ..
+if [ ! -f Mu-Simulation/simulation ]; then
+	echo ""
+	echo "Simulation executable not found, building the simulation/digitizer."
+	git clone https://github.com/EdmondRen/Mu-Simulation.git
+	cd Mu-Simulation
+	./install --cmake
+	./install_digitizer
+fi
+if ! command -v pytracker &> /dev/null; then
+	echo ""
+	echo "Tracker executable not found. Installing it in user directory in editing mode."
+	git clone https://github.com/EdmondRen/pyTracker.git
+	cd pyTracker
+	pip install -e . --user
+fi
+popd
+# Now we have the simulation and digitizer. Make an alias for them
+# The tracker can be run directly with command pytracker
+simulation=`realpath ../Mu-Simulation/simulation`
+digitizer=`realpath ../Mu-Simulation/digitizer`
+
+
+
+
+#------------------------------------------------------------------
 # Cosmic
-# setup
 PATH_COSMIC_in=$PATH_DATA/SimInput/cosmic
 PATH_COSMIC_out=$PATH_DATA/SimOutput/cosmic
 PATH_COSMIC_tmp=$PATH_DATA/temp/cosmic/
@@ -23,9 +58,6 @@ if [ ! -f cosmic/parma_cpp/cosmic_gen ]; then
 fi
 # Set the output path of PARMA
 ln -s  $PATH_COSMIC_tmp cosmic/parma_cpp/GeneOut
-
-
-
 # ----------------------------------------------------------------
 
 
