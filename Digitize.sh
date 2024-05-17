9ecabf3cc8a235337a112bfac785dbebd0fa3384
Gabriel Owh <gabriel.owh@gmail.com>
2024-05-17 15:53:52 -0700

    Working Version

--- Digitize.sh
+++ Digitize.sh
@@ -3,20 +3,19 @@
 #SBATCH --account=def-mdiamond
 #SBATCH --mem=2G
 
-if [ $# -ne 2 ]; then
-	echo "Usage: <LHCDataDirectory> <OutputDirectory> -c <CosmicDataDirectory>"
-	exit 1
-fi
-
+# ------------------------------------------------------------------------------------------------
+# Cosmic variables
 cosmic=false
 cosmic_dir=""
 
+# ------------------------------------------------------------------------------------------------
 # Checking that options are valid and if cosmic input is being used
 while getopts "c:" opt; do
 	case $opt in 
 		c)
 			cosmic_dir="$OPTARG"
 			cosmic=true
+			echo "cosmic"
 			;;
 		\?)
 			echo "Invalid Option: ${OPTARG}"
@@ -30,20 +29,29 @@ while getopts "c:" opt; do
 			;;
 	esac
 done
+shift $((OPTIND -1))
 
+# ------------------------------------------------------------------------------------------------
+# Checking and setting files/variables
+if [ $# -ne 2 ]; then
+	echo "Usage: <LHCDataDirectory> <OutputDirectory> -c <CosmicDataDirectory>"
+	exit 1
+fi
 # sourcing init.sh"
 echo "sourcing init.sh"
 source init.sh
 
-cp par_cards/par_card.txt ${simulation_dir}/Digitizer/run/
+# Change par_card.txt of digitizer to this one
+cp par_cards/par_card.txt ${digitizer_dir}/Digitizer/run/
 
+# ------------------------------------------------------------------------------------------------
+# Running digitizer
 pushd ${digitizer_dir}
 
 # Run the Digitizer
 echo "Running Digitizer"
 # Don't know exactly the name of the G4 output root file (dependent on date)
 find "${1}" -type f -name "*.root" | while read -r file; do
-	mkdir ${Path_Digi_out}/${SLURM_ARRAY_TASK_ID}
 	if $cosmic; then
 		./digitizer -l $file -c $cosmic_dir  -o ${2}
 	else 
