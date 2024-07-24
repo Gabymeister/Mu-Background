771da6b654dfc13de4477f1a2b107150243f10c7
Gabriel Owh <gabriel.owh@gmail.com>
2024-07-24 12:41:24 -0700

    Updating to work with other scripts, preserving Simulation data

--- full_sim.sh
+++ full_sim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=6:00:00
-#SBATCH --account=def-mdiamond
-#SBATCH --array=1-50
+#SBATCH --time=12:00:00
+#SBATCH --account=rrg-mdiamond
+#SBATCH --array=1-100
 #SBATCH --mem=2G
 #SBATCH --job-name=mu-fullsim
 #SBATCH --output=scripts/slurm_log/log-mu-fullsim_%a.out
@@ -10,9 +10,9 @@ source init.sh
 
 # one cosmic root file per lhc root file
 echo "Generating LHC root file"
-./run_lhc_muon.sh  30000 5
+./run_lhc_muon.sh 15
 echo "Generating cosmic root file"
-./run_cosmic.sh -n 30000 -r ${SLURM_ARRAY_TASK_ID} -f -s Run
+./run_cosmic.sh -n 1000 -r ${SLURM_ARRAY_TASK_ID} -f -s Run
 
 echo "Digitizing LHC and Cosmic files"
 if [ ! -d digi_out/${SLURM_ARRAY_TASK_ID} ]; then
@@ -20,8 +20,8 @@ if [ ! -d digi_out/${SLURM_ARRAY_TASK_ID} ]; then
 fi
 ./Digitize.sh -c ${PATH_COSMIC_out}/run_${SLURM_ARRAY_TASK_ID} ${PATH_MG5_out}/bkg_muon_${SLURM_ARRAY_TASK_ID}
 
-echo "removing G4 output directories"
-rm -rf ${PATH_COSMIC_out}/run_${SLURM_ARRAY_TASK_ID}
-rm -rf ${PATH_MG5_out}/bkg_muon_${SLURM_ARRAY_TASK_ID}
+#echo "removing G4 output directories"
+#rm -rf ${PATH_COSMIC_out}/run_${SLURM_ARRAY_TASK_ID}
+#rm -rf ${PATH_MG5_out}/bkg_muon_${SLURM_ARRAY_TASK_ID}
 
 
